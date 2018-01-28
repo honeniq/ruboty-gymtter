198fc7e4dd29ca7ace14b796de7712730be00b8f
honeniq <honeniq@gmail.com>
2018-01-28 13:39:29 +0900

    ジムに行った回数を教えてくれるようにする

--- lib/ruboty/handlers/gymtter.rb
+++ lib/ruboty/handlers/gymtter.rb
@@ -1,4 +1,5 @@
 require "ruboty/gymtter/actions/gym"
+require "ruboty/gymtter/actions/count"
 
 module Ruboty
   module Handlers
@@ -6,12 +7,15 @@ module Ruboty
     class Gymtter < Base
       on /.*?gym/, name: 'gym', description: 'ジムと言われたらえらいと応える'
       on /.*?ジム/, name: 'gym', description: 'ジムと言われたらえらいと応える'
-
+      on /count/, name: 'count', description: 'これまでの通算回数を返す'
 
       def gym(message)
         Ruboty::Gymtter::Actions::Gym.new(message).call
       end
 
+      def count(message)
+        Ruboty::Gymtter::Actions::Count.new(message).call
+      end
     end
   end
 end
