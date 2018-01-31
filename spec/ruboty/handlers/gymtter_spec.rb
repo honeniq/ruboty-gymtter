588fa6744976968c1d67273129ab2526b671355e
honeniq <honeniq@gmail.com>
2018-01-31 21:13:55 +0900

    anniversaryのテスト追加

--- spec/ruboty/handlers/gymtter_spec.rb
+++ spec/ruboty/handlers/gymtter_spec.rb
@@ -8,39 +8,73 @@ describe Ruboty::Handlers::Gymtter do
       Ruboty::Robot.new
     end
 
-  shared_context 'alice to ruboty' do
-    let(:message) do 
-      {
-       body: 'えらい @alice',
-        from: '#general',
-        to: 'alice',
-         original: {
-           body: '@ruboty gym',
-           from: 'alice',
-           robot: robot,
-           to: '#general',
-         },
-      }
-    end
-  end
-
-  describe '#gym' do
-    describe 'keyword gym' do
-      include_context 'alice to ruboty'
-      it 'gym' do
-        expect(robot).to receive(:say).with(message)
-        robot.receive(body: "@ruboty gym", from: 'alice', to: '#general')
+    shared_context 'alice to ruboty' do
+      let(:message) do 
+        {
+         body: 'えらい @alice',
+          from: '#general',
+          to: 'alice',
+           original: {
+             body: '@ruboty gym',
+             from: 'alice',
+             robot: robot,
+             to: '#general',
+           },
+        }
       end
     end
+  
+    describe '#gym normal pattern' do
+      describe 'gym' do
+        include_context 'alice to ruboty'
+        it 'gymと言われたらえらいと返す' do
+          message[:original][:body] = '@ruboty gym'
+          expect(robot).to receive(:say).with(message)
+          robot.receive(body: "@ruboty gym", from: 'alice', to: '#general')
+        end
+      end
+  
+      describe 'ジム' do
+        include_context 'alice to ruboty'
+        it 'ジムと言われたらえらいと返す' do
+          message[:original][:body] = '@ruboty ジム'
+          expect(robot).to receive(:say).with(message)
+          robot.receive(body: '@ruboty ジム', from: 'alice', to: '#general')
+        end
+      end
+  
+      describe '昨日ジム行ってきた' do
+        include_context 'alice to ruboty'
+        it '前後に他の言葉が入ってても、ジムと言われたらえらいと返す' do
+          message[:original][:body] = '@ruboty 昨日ジム行ってきた'
+          expect(robot).to receive(:say).with(message)
+          robot.receive(body: '@ruboty 昨日ジム行ってきた', from: 'alice', to: '#general')
+        end
+      end
+    end
+  
+    describe '#gym anniversary pattern' do
+      describe '10の倍数だったら「○回目 えらい」と返す' do
+        include_context 'alice to ruboty'
 
-    describe 'keyword ジム' do
-      include_context 'alice to ruboty'
-      it 'gym2' do
-        message[:original][:body] = '@ruboty ジム'
-        expect(robot).to receive(:say).with(message)
-        robot.receive(body: '@ruboty ジム', from: 'alice', to: '#general')
+        it '10回目' do
+          robot.brain.data['alice.counter'] = 9
+          
+          message[:original][:body] = '@ruboty gym'
+          message[:body] = '10回目 えらい @alice'
+          expect(robot).to receive(:say).with(message)
+          robot.receive(body: "@ruboty gym", from: 'alice', to: '#general')
+        end
+
+        it '70回目' do
+          robot.brain.data['alice.counter'] = 69
+          
+          message[:original][:body] = '@ruboty gym'
+          message[:body] = '70回目 えらい @alice'
+          expect(robot).to receive(:say).with(message)
+          robot.receive(body: "@ruboty gym", from: 'alice', to: '#general')
+        end
       end
     end
   end
-end
 end
