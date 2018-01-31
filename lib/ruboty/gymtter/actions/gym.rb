96a94cb4c6a8c983993ef17683ff7fbe95da9441
honeniq <honeniq@gmail.com>
2018-01-31 21:30:08 +0900

    ジム回数加算の部分をメソッド分けた

--- lib/ruboty/gymtter/actions/gym.rb
+++ lib/ruboty/gymtter/actions/gym.rb
@@ -10,16 +10,22 @@ module Ruboty
 
         private
         def gym
+          name = message.from_name
+          count = increment(name)
+
+          if anniversary?(count)
+            count.to_s + '回目 えらい @' + name
+          else
+            'えらい @' + name
+          end
+        end
+
+        def increment(name)
           brain = message.robot.brain
-          key = message.from_name + '.counter'
+          key = name + '.counter'
 
           brain.data[key] ||= 0
           brain.data[key] = brain.data[key] + 1
-          if anniversary?(brain.data[key])
-            brain.data[key].to_s + '回目 えらい @' + message.from_name
-          else
-            'えらい @' + message.from_name
-          end
         end
 
         def anniversary?(counter)
